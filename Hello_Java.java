1ba7a4d213a7c203a50e2f929afc6ca40d90b7bc
Jae Kiim <jaekim7171@gmail.com>
2020-11-02 12:52:52 -0800

    changed class name to Hello_Java

--- Hello_Java.java
+++ Hello_Java.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Hello_Java {
     public static void main(String[] args) {
         // Print "Hello, World" in the terminal windows.
         System.out.println("Hello, World");
