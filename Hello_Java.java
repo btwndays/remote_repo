9abba18dce4f4de981581e39ab208f1bca02b912
Jae Kiim <jaekim7171@gmail.com>
2020-11-02 14:31:12 -0800

    deleted hell_java.java file

--- Hello_Java.java
+++
@@ -1,6 +0,0 @@
-public class Hello_Java {
-    public static void main(String[] args) {
-        // Print "Hello, World" in the terminal windows.
-        System.out.println("Hello, World");
-    }
-}
